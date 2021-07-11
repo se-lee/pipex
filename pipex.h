22203f22ec757bb9e6e9e7da9e6e8c4e47be0dbb
selee <selee@student.42lyon.fr>
2021-07-11 20:48:43 +0200

    cloned libft; get_command function; before testing

--- pipex.h
+++ pipex.h
@@ -5,11 +5,26 @@
 # include <stdio.h>
 # include <fcntl.h>
 # include <sys/wait.h>
+# include <stdlib.h>
+# include "libft/libft.h"
 
 # define FILE1	argv[1]
 # define CMD1	argv[2]
 # define CMD2	argv[3]
 # define FILE2	argv[4]
+
+
+/*
+struct for cmd?
+int execve(const char *path, char *const argv[], char *const envp[]);
+*/
+
+typedef struct	s_cmd
+{
+	const char	*path;
+	char *const *argv;
+	char *const	*envp;
+}				t_cmd;
 
 int	redirect_input(const char *file);
 int	redirect_output(const char *file);
@@ -26,8 +41,6 @@ argv[2] = cmd1	---> execve
 argv[3] = cmd2	---> execve
 argv[4] = file2	---> redirection; const char *file
 
-- rename argv[] to file1, cmd1, cmd2, file2
 - need to think through how to do cmd part. 
-
 
 */
--- pipex_cmd.c
+++ pipex_cmd.c
@@ -2,10 +2,30 @@
 
 //int execve(const char *path, char *const argv[], char *const envp[]);
 // what is the path? /bin/[cmd], /sbin/[cmd] --> the command given as *path will run
+//		how many paths are there ? how to cover all ?
 // argv -- argv[0]: the command, argv[1, 2, ...]: options, don't forget to end with NULL to make sure it's the end;
 
+int	get_command(char *cmd_str, t_cmd *cmd)
+{
+	char **cmd_sep;
 
-// find command
+	cmd_sep = ft_split(cmd_str, ' ');
+	if (cmd_sep == NULL)
+		return (-1);
+	cmd->path = ft_strjoin("/bin/", cmd_sep[0]);
+	cmd->argv = cmd_sep;
+	cmd->envp = 0; // <--- ??? i dont know what to put here.
+
+	return (0);
+}
+
+void	do_command(t_cmd cmd)
+{
+	execve(cmd.path, cmd.argv, cmd.envp);
+
+	//error case ?
+}
+
 /*
 
 eg.
@@ -16,9 +36,6 @@ ls -> argv[0]
 NULL -> argv[2]
 
 maximum number of options?
-
-do split? with sep " ";
-
 
 put argv[0](==cmd name) to path so that it runs the cmd
 strjoin? with /bin/ ?
