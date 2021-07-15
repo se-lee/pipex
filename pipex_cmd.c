3cec89dff30f590449149345fefa9206c094db5c
selee <selee@student.42lyon.fr>
2021-07-16 01:26:39 +0200

    i dont know what i am doing. i'm a big mess. i just want to fix the leaks that's my only simple wish. it's getting late and iam sleepy. i can only wish my future-self will solve all the problem tomorrow. oyasumi

--- pipex_cmd.c
+++ pipex_cmd.c
@@ -11,20 +11,23 @@ int	get_command(char *cmd_str, char **env, t_cmd *cmd)
 	cmd->sep_cmd = ft_split(cmd_str, ' '); //malloc
 	if (cmd->sep_cmd == NULL)
 		return (-1);
-	while (env[i] != NULL && path != env[i]) //start of the string
+	while (env[i] != NULL && path == NULL && path != env[i]) //start of the string
 	{
 		path = ft_strstr(env[i], "PATH="); 
 		i++;
 	}
-	temp = path;
 	path = ft_substr(path, 5, (ft_strlen(path) - 5)); //malloc
-	free(temp);
 	cmd->sep_path = ft_split(path, ':'); //malloc
+	free(path);
 	i = 0;
 	while (cmd->sep_path[i] != NULL) //sep_path malloc and free problem
 	{/*make a new function*/
+		temp = cmd->sep_path[i];
 		cmd->sep_path[i] = ft_strjoin(cmd->sep_path[i], "/"); //malloc
+		free(temp);
+		temp = cmd->sep_path[i];
 		cmd->sep_path[i] = ft_strjoin(cmd->sep_path[i], cmd->sep_cmd[0]); //malloc
+		free(temp);
 		i++;
 	}
 	return (0);
@@ -40,6 +43,7 @@ void	do_command(char *argv, char **env)
 	while (cmd.sep_path[i] != NULL)
 	{
 		execve(cmd.sep_path[i], cmd.sep_cmd, env);
+		free(cmd.sep_path[i]);
 		i++;
 	}
 //free cmd;
